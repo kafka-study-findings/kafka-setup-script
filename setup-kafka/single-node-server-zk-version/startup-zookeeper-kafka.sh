ae7b697bf12f55d395f3f470be85b0c496ff5267
xethhung <adamhung12@gmail.com>
2023-01-17 15:03:24 +0800

    first draft of single node os version

---
+++ setup-kafka/single-node-server-zk-version/startup-zookeeper-kafka.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd kafka_binary_package
+
+# Start zookeeper
+nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zk.log 2>&1 &
+
+# Start kafka
+nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
+
+tail -f zk.log | sed 's/^/zk.log: /' & tail -f kafka.log | sed 's/^/kafka.log: /'
